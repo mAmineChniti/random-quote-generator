e810b71355de3d5bda95f0b8f1c7d710a36d6e60
mAmineChniti <emin.chniti@esprit.tn>
2024-06-01 22:40:01 +0100

    linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,8 +3,11 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/stylistic-type-checked',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules', 'src/_codux/'],
   parser: '@typescript-eslint/parser',
